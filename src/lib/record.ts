ebbbd882a090740d623bc3bdcacfb92cad8b0492
pewww <jung22337@naver.com>
2020-08-30 23:23:04 +0900

    :construction: Recording 확인을 위한 임시 클래스 추가

---
+++ src/lib/record.ts
@@ -0,0 +1,38 @@
+import MediaRecorder from '../@types/mediaRecorder';
+
+export default class Record {
+  mediaRecorder: MediaRecorder;
+  audioChunks: Blob[];
+
+  constructor(stream: MediaStream) {
+    this.mediaRecorder = new window.MediaRecorder(stream);
+    this.audioChunks = [];
+
+    this.onWatchRecording();
+    this.onStopWatchingRecording();
+  }
+
+  public startRecording() {
+    this.mediaRecorder.start();
+  }
+
+  public stopRecording() {
+    this.mediaRecorder.stop();
+  }
+
+  private onWatchRecording() {
+    this.mediaRecorder.addEventListener('dataavailable', event => {
+      this.audioChunks.push(event.data);
+    });
+  }
+
+  private onStopWatchingRecording() {
+    // 임시 로직
+    this.mediaRecorder.addEventListener('stop', () => {
+      const audioBlob = new Blob(this.audioChunks);
+      const audioUrl = URL.createObjectURL(audioBlob);
+      const audio = new Audio(audioUrl);
+      audio.play();
+    });
+  }
+}
